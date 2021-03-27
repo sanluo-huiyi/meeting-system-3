45fee96cc0f6e487298698e15e506252e2832aeb
陈皓宇 <44268299+dqcx2411@users.noreply.github.com>
2021-03-27 16:10:31 +0800

    Update Secretary.java

--- client/src/fzu/zrf/mtsys/client/gui/Secretary.java
+++ client/src/fzu/zrf/mtsys/client/gui/Secretary.java
@@ -1,14 +1,158 @@
 package fzu.zrf.mtsys.client.gui;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+/**
+ * @ClassName serDemo
+ * @Description TODO
+ * @Author Charley Chen
+ * @DATE 2021/3/27 15:15
+ * @Version 1.0
+ **/
 public class Secretary extends Application {
+    private final TableView<Person> table = new TableView<>();
+
+    // 数据源
+    private final ObservableList<Person> data =
+            FXCollections.observableArrayList(
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"),
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"),
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"),
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"),
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com"),
+                    new Person("Jacob", "Smith", "jacob.smith@example.com"),
+                    new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+                    new Person("Ethan", "Williams", "ethan.williams@example.com"),
+                    new Person("Emma", "Jones", "emma.jones@example.com"),
+                    new Person("Michael", "Brown", "michael.brown@example.com")
+            );
+
 
     @Override
-    public void start(Stage var1) throws Exception {
-        // TODO 自动生成的方法存根
+    public void start(Stage stage) {
+        String fenluntanName="";
+        Scene scene = new Scene(new Group());
+        stage.setTitle("秘书，用户列表");
+        stage.setWidth(450);
+        stage.setHeight(500);
 
+        final Label label = new Label("分论坛"+fenluntanName+"参会者信息");
+        label.setFont(new Font("Arial", 20));
+
+        table.setEditable(true);
+
+        // 每个Table的列
+        TableColumn firstNameCol = new TableColumn("First Name");
+        // 设置宽度
+        firstNameCol.setMinWidth(100);
+        // 设置分箱的类下面的属性名
+        firstNameCol.setCellValueFactory(
+                new PropertyValueFactory<>("firstName"));
+
+        TableColumn lastNameCol = new TableColumn("Last Name");
+        lastNameCol.setMinWidth(100);
+        lastNameCol.setCellValueFactory(
+                new PropertyValueFactory<>("lastName"));
+
+        TableColumn emailCol = new TableColumn("Email");
+        emailCol.setMinWidth(200);
+        emailCol.setCellValueFactory(
+                new PropertyValueFactory<>("email"));
+
+        // 设置数据源
+        table.setItems(data);
+        // 一次添加列进TableView
+        table.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+        Button btn = new Button();
+        btn.setText("添加消息");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");  // wait to be overrided 点一下会跳出新增消息的窗口
+            }
+        });
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table,btn);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public static class Person {
+
+        private final SimpleStringProperty firstName;
+        private final SimpleStringProperty lastName;
+        private final SimpleStringProperty email;
+
+        private Person(String fName, String lName, String email) {
+            this.firstName = new SimpleStringProperty(fName);
+            this.lastName = new SimpleStringProperty(lName);
+            this.email = new SimpleStringProperty(email);
+        }
+
+        public String getFirstName() {
+            return firstName.get();
+        }
+
+        public void setFirstName(String fName) {
+            firstName.set(fName);
+        }
+
+        public String getLastName() {
+            return lastName.get();
+        }
+
+        public void setLastName(String fName) {
+            lastName.set(fName);
+        }
+
+        public String getEmail() {
+            return email.get();
+        }
+
+        public void setEmail(String fName) {
+            email.set(fName);
+        }
+    }
 }
+
