e17d5d9dc602b2d1e6520a7b6510baee24d1b7d6
Tozzger <997053715@qq.com>
2021-03-27 15:58:07 +0800

    数据库连接测试

--- server/src/fzu/zrf/mtsys/server/Main.java
+++ server/src/fzu/zrf/mtsys/server/Main.java
@@ -2,11 +2,15 @@ package fzu.zrf.mtsys.server;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class Main {
 
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
     static final String DB_URL = "jdbc:mysql://blog.tozzger.info:3306/";
     
     public static void main(String[] args) throws Exception {
@@ -15,7 +19,12 @@ public class Main {
         InputStream is = new FileInputStream("psw.properties");
         p.load(is);
         is.close();
-        System.out.println(p.get("db"));
+        Connection conn = DriverManager.getConnection(DB_URL + p.getProperty("db"),p.getProperty("id"),p.getProperty("psw"));
+        Statement s = conn.createStatement();
+        ResultSet rs = s.executeQuery("SELECT * FROM user_type");
+        while(rs.next()){
+            System.out.println(rs.getInt("id"));
+        }
     }
 
 }
